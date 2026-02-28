be6c10d51205c6b2b6dad619ec9e74f87f7e2577
Mirna El Tohamy <m.tohamy.2000@gmail.com>
2026-03-01 01:33:18 +0200

    part 1

--- OOPAssignment4/Program.cs
+++ OOPAssignment4/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿#region part1
+//Q1
+//Static Binding (Compile-time)
+//Happens with method overloading, depends on reference type
+
+//Dynamic Binding (Run-time)
+//happens with method overriding using inheritance, depends on the actual object type
+
+//Q2
+// Method Overloading:
+//Same method name, different parameters (number/type)
+//Happens in the same class
+//Resolved at compile time
+
+//Method Overriding:
+//Reimplementing a base class method in a derived class
+//same method signature
+//Resolved at runtime
+
+//Q3
+//virtual  -> used in base class to allow a method to be overridden
+//override -> used in derived class to provide new implementation
+#endregion
